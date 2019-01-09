d39eefa5616de8a4f0a47b87ff69aa50fd303d07
fernando <garciafertson@gmail.com>
2019-01-09 16:59:33 -0600

    ene9 19 run pan sintax

--- panapp/run_pangenome_analysis.py
+++ panapp/run_pangenome_analysis.py
@@ -1,8 +1,8 @@
 ''' the next module calls metagpn program for metagenome analisis only on cliques at 100% and 95% identity cutoff'''
-    from Bio import SeqIO
+from Bio import SeqIO
 
 def build_metapgn_input(filename):
-    gbk_filename=filename*".gbk"
+    gbk_filename=filename+".gbk"
     fna_filename=filename+".fna"
     input_handle=open(gbk_filename, "r")
     output_handle=open(fna_filename, "w")
@@ -19,5 +19,5 @@ def metapgn(self):
         cmd1=["perl", "MetaPGN_flow.pl", "-q", name+".fasta", "-qt", "genome", "-ref", ref+".fasta", "-p", "genome_genes_nt", "-gene", "assembly"] 
         sh_script = subprocess.Popen(cmd, outfile="MetaPGN.sh")
 
-        cmd2=["sh" "MetaPGN.sh" .output.standard "MetaPGN.log"   .output.error "MetaPGN.err"
+        cmd2=["sh", "MetaPGN.sh", ".output.standard", "MetaPGN.log",  " .output.error", "MetaPGN.err"]
 
